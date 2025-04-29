b7040b24d6eef45f44ef732c0042a3ff72c81ff9
Autumniya <43678984+Autumniya@users.noreply.github.com>
2025-04-29 17:06:41 +0000

    Update celery_worker.py

--- bambara-tts-api/celery_worker.py
+++ bambara-tts-api/celery_worker.py
@@ -4,7 +4,7 @@ import os
 import uuid
 from tempfile import gettempdir
 
-celery_app = Celery("tts_tasks", broker="amqp://guest:guest@localhost:5672//")
+celery_app = Celery("tts_tasks", broker=os.getenv("REDIS_URL", "redis://localhost:6379/0"))
 
 # Load model only once for the worker
 tts_model = TTS(model_name="tts_models/bam/fairseq/vits").to("cpu")
