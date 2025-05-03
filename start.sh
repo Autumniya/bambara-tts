e7bd52606df207541ce142cc17849a91c3a29747
Autumniya <43678984+Autumniya@users.noreply.github.com>
2025-05-03 11:47:37 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Start Celery in background
-celery -A celery_worker.celery_app worker --loglevel=info &
+celery -A app.celery worker --loglevel=info &
 # Start Gunicorn
 exec gunicorn app:app --bind 0.0.0.0:8080 --timeout 120
